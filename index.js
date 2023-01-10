f4e2bb68ca22fbec0f76e4af00d1163c072d06d2
csebij1996 <122315662+csebij1996@users.noreply.github.com>
2023-01-10 17:04:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.send('Na végre kész!');
+    res.send('Helló világ!');
 })
 
 app.listen(port, () => {
